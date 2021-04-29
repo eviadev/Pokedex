9e7ebb02231c72b962d3dd8c22639f5804b08647
eviadev <eviadev@users.noreply.github.com>
2021-04-30 00:44:09 +0200

    rebase

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './components/App';
-import Router from './router'
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Router />, document.getElementById('root'));
-registerServiceWorker();
